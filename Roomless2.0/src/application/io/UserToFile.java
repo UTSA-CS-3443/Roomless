f0893e8fabd670ef8bb6b20207ab9cb5542ad4b3
Nick <nicksimmons53@gmail.com>
2017-12-01 15:08:47 -0600

    Include the toFile method.

---
+++ Roomless2.0/src/application/io/UserToFile.java
@@ -0,0 +1,43 @@
+package application.io;
+
+import java.io.*;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+
+/**
+ * Class that sends the new user info from the test to a text file. 
+ * 
+ * @author nsimm
+ *
+ */
+public class UserToFile {	
+	
+	/**
+	 * The method that creates the new BufferedWriter and file. 
+	 * 
+	 * @param test
+	 */
+	public static void toFile(String userName, String email) {
+		Formatter output = null;
+		
+		try { 
+			output = new Formatter("data.txt");
+		} catch (SecurityException e) {
+			System.err.println("Write permission denied.");
+			System.exit(1);
+		} catch (FileNotFoundException e) {
+			System.err.println("File not found.");
+			System.exit(1);
+		}
+		
+		try {
+			output.format("%s,%s", userName, email);
+		} catch(FormatterClosedException e) {
+			System.err.println("Error writing to file.");
+		}
+		
+		if (output != null) {
+			output.close( );
+		}
+	}
+}
