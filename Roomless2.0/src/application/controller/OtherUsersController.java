85665810f7545d92305f9b8a41aefb81531765c0
Stephanie Callahan <stephanie@sine-nomine>
2017-12-03 20:24:19 -0600

    Fixed the view others / test buttons

---
+++ Roomless2.0/src/application/controller/OtherUsersController.java
@@ -0,0 +1,59 @@
+package application.controller;
+
+import application.model.Test;
+import java.awt.Button;
+import java.awt.TextField;
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Slider;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import application.io.*;
+import application.test.UsersTest;
+
+/**
+ * View other users in the system To be immediately followed by the application:
+ * test page and login page
+ * 
+ * @author stephanie
+ *
+ */
+
+public class OtherUsersController implements EventHandler<ActionEvent> {
+
+	/**
+	 * Go to the main screen
+	 */
+
+	@FXML
+	public void viewMain() {
+		try {
+			Parent root3 = FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
+			Main.stage.setScene(new Scene(root3, 700, 500));
+			Main.stage.show();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		// Not sure what goes here yet
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Roomless"));
+			Main.stage.setScene(new Scene(root, 700, 500));
+			Main.stage.show();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+
+	}
+
+}
